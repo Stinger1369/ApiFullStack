76084d7894642cb930572053f0bf7df7dad15100
mToan <mtoanp@outlook.com>
2023-12-06 11:00:01 +0100

    2023-12-06-11h-mto-chatbot-toggle
    chatbot toggle

--- MultiApi/src/components/ChatbotComponent/ChatbotComponent.jsx
+++ MultiApi/src/components/ChatbotComponent/ChatbotComponent.jsx
@@ -6,15 +6,43 @@ import { useAuth } from '../../contexts/AuthContext';
 import config from '../../utils/chatbotConfig';
 import MessageParser from '../../utils/MessageParser';
 import ActionProvider from '../../utils/ActionProvider';
+import './ChatbotComponent.scss'
 
 const ChatbotComponent = () => {
   const { isAuthenticated } = useAuth();
+  let collapse = true
+  const handleCollapse = () => {
+    collapse = !collapse
+    let chatbot = document.querySelector('.draggable-chatbot-container')
+    // let chatbotContent = document.querySelector('.chatbot-content')
+    let chatControl = document.querySelector('#chat-control')
+    if(collapse) {
+      chatbot.style.height = '25px';
+      // chatbotContent.style.height = 0;
+      chatControl.classList.remove('bi-chevron-down')
+      chatControl.classList.add('bi-chevron-up')
+    } else {
+      chatbot.style.height = '580px';
+      // chatbotContent.style.height = '530px';
+      chatControl.classList.remove('bi-chevron-up')
+      chatControl.classList.add('bi-chevron-down')
+    }
+  }
 
   return (
     <Draggable>
       <div className="draggable-chatbot-container">
         <div className="chatbot-window">
-          <div className="chatbot-header">Chatbot</div>
+
+          <div className="chatbot-header" onClick={handleCollapse} >
+            <div className="title">Chatbot</div>
+            <div className="chatbot-toggle btn" 
+                // data-bs-toggle="collapse"  data-bs-target="#chatbot-content"
+            >
+              { collapse ? <i id="chat-control" className="bi bi-chevron-up"></i> : <i id="chat-control" className="bi bi-chevron-down"></i> }
+            </div>
+          </div>
+
           <div className="chatbot-content">
             {isAuthenticated ? (
               // Si l'utilisateur est connecté, affichez le message de bienvenue
@@ -24,6 +52,7 @@ const ChatbotComponent = () => {
               <div>Veuillez vous connecter pour utiliser le chatbot.</div>
             )}
           </div>
+
         </div>
       </div>
     </Draggable>
