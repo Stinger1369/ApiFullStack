6dcef4385850331a6b6595642207bf01782a6ae2
mToan <mtoanp@outlook.com>
2023-12-06 14:23:10 +0100

    2023-12-06-14h20-mto-background-animation
    bgs

--- MultiApi/src/App.jsx
+++ MultiApi/src/App.jsx
@@ -36,6 +36,8 @@ function App() {
   console.warn(theme);
   return (
     <div className="App">
+    <div className="cloud cloud-1"></div>
+    <div className="cloud cloud-2"></div>
     <AuthProvider>
       <ThemeProvider>
         <ThemeControl />
