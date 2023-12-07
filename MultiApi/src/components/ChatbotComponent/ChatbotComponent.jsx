f09b394680f1403992dcc6d0a8d69d49559f1cdb
Dev6Socializus <72788978+Stinger1369@users.noreply.github.com>
2023-12-07 11:08:48 +0100

    2023-12-07-11h07-Bilelanimation
    changement d'etat du gif et png

--- MultiApi/src/components/ChatbotComponent/ChatbotComponent.jsx
+++ MultiApi/src/components/ChatbotComponent/ChatbotComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Draggable from 'react-draggable';
 import Chatbot from 'react-chatbot-kit';
 import 'react-chatbot-kit/build/main.css';
@@ -7,33 +7,41 @@ import config from '../../utils/chatbotConfig';
 import MessageParser from '../../utils/MessageParser';
 import ActionProvider from '../../utils/ActionProvider';
 import './ChatbotComponent.scss';
-import clickSound from '../../assets/sounds/chatbot.mp3'; // Assurez-vous que le chemin est correct
+import clickSound from '../../assets/sounds/chatbot.mp3';
+import gifPath from '../../assets/images/chatbot-unscreen.gif';
+import pngPath from '../../assets/images/chatbot.png';
 
 const ChatbotComponent = () => {
   const { isAuthenticated } = useAuth();
-  
+  const [collapse, setCollapse] = useState(true);
+  const [showGif, setShowGif] = useState(true);
 
   const audio = new Audio(clickSound);
 
   // Fonction pour jouer le son
   const playSound = () => {
     audio.play();
-  }
+  };
 
-  let collapse = true;
   const handleCollapse = () => {
-    // Bascule l'état de collapse
-    collapse = !collapse;
+    const newCollapseState = !collapse;
+    setCollapse(newCollapseState);
 
-    // Joue le son seulement si le chatbot est en train de s'ouvrir
-    if (!collapse) {
+    // Réinitialiser l'affichage du GIF à chaque ouverture
+    if (!newCollapseState) {
+      setShowGif(true);
       playSound();
+
+      // Définir un délai pour changer le GIF en PNG après 3 secondes
+      setTimeout(() => {
+        setShowGif(false);
+      }, 1800);
     }
 
     let chatbot = document.querySelector('.draggable-chatbot-container');
     let chatControl = document.querySelector('#chat-control');
 
-    if (collapse) {
+    if (newCollapseState) {
       chatbot.style.height = '25px';
       chatControl.classList.remove('bi-chevron-down');
       chatControl.classList.add('bi-chevron-up');
@@ -44,12 +52,14 @@ const ChatbotComponent = () => {
     }
   };
 
+
+
   return (
     <Draggable>
       <div className="draggable-chatbot-container">
         <div className="chatbot-window">
           <div className="chatbot-header" onClick={handleCollapse}>
-            <div className="walking-man"></div>
+            <div className="walking-man" style={{ backgroundImage: `url(${showGif ? gifPath : pngPath})` }}></div>
             <div className="chatbot-toggle btn">
               {collapse ? <i id="chat-control" className="bi bi-chevron-up"></i> : <i id="chat-control" className="bi bi-chevron-down"></i>}
             </div>
